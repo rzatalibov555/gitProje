6d0233bf0aa333e7890514ab7c982f12b33093f5
rzatalibov555 <sethubpro@gmail.com>
2024-11-01 22:13:39 +0400

    Js Outputlar

---
+++ js/main.js
@@ -0,0 +1,96 @@
+
+
+
+// function batman(){
+//     document.getElementById("imgStyle").src = "https://www.lightbulbs.com/catalog/product_line_photos/light_bulbs_beauty.jpg?123456"
+// }
+
+
+
+// function superman(){
+//     document.getElementById("imgStyle").src = "https://c8.alamy.com/comp/2A428DP/vector-3d-realistic-off-light-bulb-icon-closeup-isolated-on-white-background-design-template-clipart-glowing-incandescent-filament-lamps-2A428DP.jpg"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("word").innerHTML  = "<p style='color:red'>Hello</p>"
+
+document.getElementById("word2").innerText = "<p style='color:red'>Hello</p>"
+
+
+// document.getElementById("word2").style.background = "blue"
+// document.getElementById("word2").style.color = "white"
+
+
+
+
+
+function yoxEt(){
+    document.getElementById('divKv').style.display='none'
+}
+
+
+function activeEt(){
+    document.getElementById('divKv').style.display='block'
+}
+
+
+
+
+
+
+
+
+
+// window.alert("Demo")
+
+// document.write("JS DERSIMIZE basladiq!")
+
+// document.getElementById("word").innerHTML = "Output dersi"
+
+// console.log("Men burdayam")
+
+
+// let car = ["BMW", "Mercedes", "Audi"]
+
+// document.getElementById("word").innerHTML = car[1]
+
+// console.log(car)
+
+// document.write(15 + 26);
+
+// prompt("Enter username:")
+
+// let a = "Melumati silmek istediyinize eminsinizmi?"
+
+
+
+
+
+// function delData(){
+//     confirm(a)
+// }
+
+
+// let a = prompt("Enter username:")
+
+// if(a == "Rza"){
+//     alert("Welcome Rza")
+// }else{
+//     alert("Sehv")
+// }
