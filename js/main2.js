cde0c473bfbcee6f3d1390f9dee729cb018f6305
rzatalibov555 <sethubpro@gmail.com>
2024-11-06 21:47:48 +0400

    var, let, const (js3)

---
+++ js/main2.js
@@ -0,0 +1,161 @@
+// 'use strict'
+
+
+// var, let, const
+
+
+
+
+// a = 5
+// console.log(a)
+
+
+
+// a = 9
+// console.log(a)
+
+
+
+// var a     +
+// let a     +
+// const a   -
+// console.log(a)
+
+
+// var a = 2
+// console.log(a)
+
+// var a = 5
+// console.log(a)
+
+
+
+
+// let a = 2
+// console.log(a)
+
+// let a = 5
+// console.log(a)
+
+
+
+
+// const a = 2
+// console.log(a)
+
+// const a = 5
+// console.log(a)
+
+
+// -------------------------------------------
+
+
+// var a = 2
+// console.log(a)
+
+// a = 5
+// console.log(a)
+
+
+
+
+// let a = 2
+// console.log(a)
+
+// a = 5
+// console.log(a)
+
+
+
+
+// const a = 2
+// console.log(a)
+
+// a = 5
+// console.log(a)
+
+
+// ==========================================
+
+// array []    // massiv
+// object {}   //
+// function () // 
+
+// const a = ["BMW", "MERCEDES","TOYOTA"]
+// console.log(a)
+
+// a[2] = "Lexus"
+// console.log(a)
+
+// ==========================================
+
+
+
+// let student = "Mahammad"
+// console.log(student)
+
+// {
+//     let student = "Murtuz"
+//     console.log(student)
+// }
+
+// {
+//     let student = "Maqa"
+//     console.log(student)
+// }
+
+// console.log(student)
+
+
+// ==========================================
+
+
+
+
+// const student = "Mahammad"
+// console.log(student)
+
+// {
+//     const student = "Murtuz"
+//     console.log(student)
+// }
+
+// console.log(student)
+
+
+// {
+//     const student = "Maqa"
+//     console.log(student)
+// }
+
+// console.log(student)
+
+
+
+// const carName = "Saab";
+
+// console.log(carName)
+
+
+// function demo(){
+//     if(5 == 3){
+//         alert("ela")
+//     }else{
+//         alert("olmadi")
+//     }
+// }
+
+// demo()
+
+
+
+
+
+
+
+
+
+
+
+
+
