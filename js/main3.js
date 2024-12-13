f2bb363a38314e78cf6e93a539a005f517dfa15c
rzatalibov555 <sethubpro@gmail.com>
2024-12-14 03:43:19 +0400

    lesson this

---
+++ js/main3.js
@@ -0,0 +1,30 @@
+// function chBlue(e){
+//     e.style.background = "blue"
+// }
+
+// function chGreen(e){
+//     e.style.background = "green"
+// }
+
+// function chPurple(e){
+//     e.style.background = "purple"
+// }
+
+// function chWhite(e){
+//     e.style.display = "none"
+// }
+
+
+function selectPlayer(e){
+    document.getElementById("selected_player_img").src = "img/"+e.getAttribute("data-sekil")
+}
+
+function chBorder(e){
+    e.style.borderColor = "blue"
+    e.style.transform = "rotate(45deg)"
+}
+
+function chBorderRed(e){
+    e.style.borderColor = "red"
+    e.style.transform = "rotate(0deg)"
+}
