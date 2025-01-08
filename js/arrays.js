06ae4fa925ff917d54ba01e32d4da4d68c8cbc46
rzatalibov555 <sethubpro@gmail.com>
2025-01-08 20:48:31 +0400

    Array

---
+++ js/arrays.js
@@ -0,0 +1,141 @@
+
+
+
+// let user1 = "Eyyub"
+// let user2 = "Mehemmed"
+// let user3 = "Hesen"
+// let user4 = "Saleh"
+// let user5 = "Suleyman"
+
+
+
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+// console.log(user4)
+// console.log(user5)
+
+
+// () - Function
+// [] - Array// Massiv
+// {} - Object
+
+
+
+// let users = ["Eyyub", "Mehemmed", "Hesen", "Saleh", "Suleyman"]
+
+// console.log(users[4][0])
+
+// console.log(`Menim adim ${users[4]}dir.`)
+// document.write(`Menim adim ${users[0]}dur.`)
+// document.getElementById('demo').innerHTML = `Menim adim ${users[0]}dur.`
+
+// console.log(users[0])
+// console.log(users[1])
+// console.log(users[2])
+// console.log(users[3])
+// console.log(users[4])
+
+// associate array [[]]
+
+// let tour = [
+//     ["Eyyub", "Mehemmed", "Hesen", "Saleh", "Suleyman"],
+//     ["Aygun", "Sabina", "Ayten", "Nigar", "Mehriban", "Gultac", "Aytac"],
+//     ["Quba", "Lenkeran", "Qebele"],
+//     ["Turkey", "Almaniya", "Italiya","Ispaniya"],
+//     ["149", "150",["HTML", "CSS"]]
+// ]
+
+// console.log(tour[4][2][1])
+
+// let cars = ["BWM", "Mercedes", "Jaguar"]
+
+// console.log(tour[0][1]+"in masini "+cars[2]+"dir.")
+// console.log(tour[0][1]+"in masini "+cars[2]+"dir.")
+// console.log(`${tour[0][0]} ile ${tour[1][3]} ${tour[2][2]}ye tura ${tour[4][0]} nomreli ${cars[1]} markali avtobus ile yola duwduler.`)
+
+// =============== Homework =======================================
+
+// let tour = [
+//     ["Eyyub", "Mehemmed", "Hesen", "Saleh", "Suleyman"],
+//     ["Aygun", "Sabina", "Ayten", "Nigar", "Mehriban", "Gultac", "Aytac"],
+//     ["Quba", "Lenkeran", "Qebele"],
+//     ["Turkey", "Almaniya", "Italiya","Ispaniya"],
+//     ["149", "150",["HTML", "CSS"],"Rza"]
+// ]
+
+// H => Hesen
+// e => Mehriban
+// s => Ispaniya
+// e => Suleyman
+// n => Aygun
+
+// Menim adim Hesendir
+// console.log()
+// =============== Homework =======================================
+
+
+
+// [] ve ya Array()
+// let cars1 = new Array("BWM", "Mercedes", "Jaguar")
+// let cars2 = ["BWM", "Mercedes", "Jaguar"]
+
+
+// console.log(cars1) 
+// console.log(cars2) 
+
+
+// document.body.style.backgroundImage = "url('https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/47166A2DCA6E85A1D74A9D7A6FA2B6EB6069B613D9C8E3DB4E998AA78461B530/scale?width=1200&aspectRatio=1.78&format=webp')"
+// document.getElementsByTagName("body")[0].style.backgroundImage = "url('https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/47166A2DCA6E85A1D74A9D7A6FA2B6EB6069B613D9C8E3DB4E998AA78461B530/scale?width=1200&aspectRatio=1.78&format=webp')"
+
+
+// let cars = []
+// console.log(cars)
+
+// cars[0] = "Toyota"
+// cars[1] = "Mercedes"
+
+// console.log(cars)
+
+
+
+// let cars2 = ["BWM", "Mercedes", "Jaguar"]
+// let masin =  cars2[0]
+
+// console.log(masin)
+
+
+// function test(){
+//     alert("Function active")
+// }
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", test];
+
+// let starts = fruits[4]
+
+// starts()
+
+
+
+// let result = fruits.toString();
+// console.log(typeof result)
+
+
+// document.getElementById("demo").innerHTML = fruits.toString();
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.length)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("Armud")
+fruits[fruits.length] = "Lemon";  // Adds "Lemon" to fruits
+fruits[6] = ["Demo","nemo"];
+fruits[5] = "Potato";
+console.log(fruits)
+// let length = fruits[fruits.length - 3];
+// console.log(length)
+
+// console.log(fruits)
+// console.log(fruits.sort())
